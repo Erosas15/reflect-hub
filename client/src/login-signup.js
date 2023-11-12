c8cefe2ea4214e4e8350953645afb32776bb11cd
Erosas15 <rosaserick15@ucla.edu>
2023-11-11 17:08:38 -0800

    Created login/signup form structure
    I created a signup form with ways to switch between the sign up and login. Set up basic structure and color using css.

---
+++ client/src/login-signup.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './login-signup.css';
+
+const LoginSignup = () => {
+    const [action, setAction] = useState('Login');
+
+    const handleFormSwitch = () => {
+        setAction((prevAction) => (prevAction === 'Login' ? 'Sign Up' : 'Login'));
+    };
+
+    const handleFormSubmit = () => {
+        // Placeholder for future Firebase integration
+        if (action === 'Login') {
+            // Handle login logic
+        } else {
+            // Handle signup logic
+        }
+    };
+
+    return (
+        <div className='container'>
+            <div className='login_header'>
+                <div className='signin_text'>{action}</div>
+            </div>
+
+            <div className='inputs_container'>
+                <div className='input_box'>
+                    <input type='text' placeholder='Name'></input>
+                </div>
+                <div className='input_box'>
+                    <input type='email' placeholder='Email'></input>
+                </div>
+                <div className='input_box'>
+                    <input type='password' placeholder='Password'></input>
+                </div>
+            </div>
+
+            <div className='formSwitch'>
+                {action === 'Login' ? (
+                    <>
+                        Not signed up? <span onClick={handleFormSwitch}>Sign Up</span>
+                    </>
+                ) : (
+                    <>
+                        Already a member? <span onClick={handleFormSwitch}>Login</span>
+                    </>
+                )}
+            </div>
+
+            <div className='submitContainer'>
+                <div className='submit gray' onClick={handleFormSubmit}>
+                    {action === 'Login' ? 'Login' : 'Sign Up'}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignup;
