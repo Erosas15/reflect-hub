fb10936426237facccae94b765435b3efcc20fed
X_Laptop <xandnea@gmail.com>
2023-11-12 12:28:06 -0800

    Set up and added stylization to landing-page as well as connected the login button on landing page to the login-signup page

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LandingPage from './landing-page';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
