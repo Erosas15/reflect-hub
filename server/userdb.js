4ac30b8c86cba70923f4bd86db63e491f26df40c
Ashvin Loghashankar <ashvinloghs@Ashvins-MacBook-Air-2.local>
2023-11-16 13:05:23 -0800

    Added the code for adding a journal entry to the newly setup databse

--- server/userdb.js
+++ server/userdb.js
@@ -40,4 +40,33 @@ const getFromDB = async (userID) => {
     });
 };
 
-module.exports = { pushToDB, getFromDB };
+const addJournalEntry = async (userID, time, entry) => {
+  // Reference to the specific user document
+  const userDocRef = doc(colRef, userID);
+
+  // Reference to the "orders" subcollection within the user document
+  const journalCollection = collection(userDocRef, "journalHistory");
+
+  // Data to add to the "orders" subcollection
+  const journalData = {
+    timeStamp: time,
+    entry: entry,
+  };
+
+  // Use addDoc to add a new document to the "orders" subcollection
+  addDoc(journalCollection, journalData)
+    .then((newDocRef) => {
+      console.log(
+        `Document added to orders subcollection with ID: ${newDocRef.id}`
+      );
+      // You can perform additional actions after adding the document if needed
+    })
+    .catch((error) => {
+      console.error(
+        "Error adding document to orders subcollection:",
+        error.message
+      );
+    });
+};
+
+module.exports = { pushToDB, getFromDB, addJournalEntry};
