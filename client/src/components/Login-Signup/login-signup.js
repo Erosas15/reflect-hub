5c2cf54b6c4f3fb1ea6e6aa5c153dd2210e3bff8
Sragvi Vadali <reachsragvi@gmail.com>
2023-12-06 19:11:19 -0800

    Add confirmation password for sign up form

--- client/src/components/Login-Signup/login-signup.js
+++ client/src/components/Login-Signup/login-signup.js
@@ -4,139 +4,161 @@ import "./login-signup.css";
 import Footer from "../Footer/footer";
 
 const LoginSignup = () => {
-    const [isLogin, setIsLogin] = useState(true);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
 
-    
-    const handleFormSwitch = () => {
-        setIsLogin(!isLogin);
-    };
+  const handleFormSwitch = () => {
+    setIsLogin(!isLogin);
+  };
 
-    const handleFormSubmit = async() => {
-        try {
+  const handleFormSubmit = async () => {
+    try {
+      if (!isLogin && password !== confPassword) {
+        alert("Passwords do not match");
+        return; // Do not proceed with form submission
+      }
+      const config = {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
 
-            const config = {
-                withCredentials: true,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            };
-    
+      if (isLogin) {
+        const response = await axios.post(
+          "http://localhost:3001/auth/api/signin",
+          { email, password },
+          config
+        );
 
-            if (isLogin){
-                const response = await axios.post(
-                    "http://localhost:3001/auth/api/signin",
-                    { email, password },
-                    config
-                  );
-          
-                  if (response.data.success) {
-                    const { userId } = response.data;
-          
-                    // Save user.uid separately for future usage
-                    localStorage.setItem("userID", userId);
-                    window.location.href = "journal";
-                  } else {
-                    // Handle unsuccessful login
-                    console.error(response.data.error);
-                  }
-            }else {
-                const response = await axios.post(
-                    "http://localhost:3001/auth/api/signup",
-                    { name, email, password },
-                    config
-                  );
-          
-                  if (response.data.success) {
-                    const { userId } = response.data;
-          
-                    // Save user ID separately for future usage
-                    localStorage.setItem("userID", userId);
-          
-                    window.location.href = "/";
-                 } else {
-                    // Handle unsuccessful signup
-                    console.error(response.data.error);
-                  }      
-            }
-        } catch (error){
-            console.error('error during submission',error.message);
+        if (response.data.success) {
+          const { userId } = response.data;
+
+          // Save user.uid separately for future usage
+          localStorage.setItem("userID", userId);
+          window.location.href = "/";
+        } else {
+          // Handle unsuccessful login
+          console.error(response.data.error);
         }
+      } else {
+        const response = await axios.post(
+          "http://localhost:3001/auth/api/signup",
+          { name, email, password },
+          config
+        );
 
-    };
+        if (response.data.success) {
+          window.location.href = "/";
+        } else {
+          // Handle unsuccessful signup
+          console.error(response.data.error);
+        }
+      }
+    } catch (error) {
+      console.error("error during submission", error.message);
+    }
+  };
 
-    return (
+  return (
+    <div className="page">
+      {isLogin ? (
+        <div className="container">
+          <div className="login_header">
+            <div className="signin_text">Login</div>
+          </div>
 
-        <div className='page'>
-        {isLogin ? (
-            <div className='container'>
-                <div className='login_header'>
-                    <div className='signin_text'>Login</div>
-                </div>
-                
-                <div className='inputs_container'>
-                    <div className='input_box'>
-                        <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <div className="inputs_container">
+            <div className="input_box">
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input_box">
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
 
-                    </div>
-                    <div className='input_box'>
-                        <input type='email' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    </div>
-                </div>
+          <div className="formSwitch">
+            <>
+              Not signed up? <span onClick={handleFormSwitch}>Sign Up</span>
+            </>
+          </div>
 
-                <div className='formSwitch'>
-                    <>
-                    Not signed up? <span onClick={handleFormSwitch}>Sign Up</span>
-                    </>
-                </div>
-
-                <div className='submitContainer'>
-                    <div className='submit gray' onClick={handleFormSubmit}>
-                        Login
-                    </div>
-                </div>
-
-            </div>      
-        ) : (
-            <div className='container'>
-                <div className='signup_header'>
-                    <div className='signup_text'>Sign up</div>
-                </div>
-
-                <div className='inputs_container'>
-                    <div className='input_box'>
-                        <input type='text' placeholder='Name' value={name} onChange ={(e) => setName(e.target.value)}/>
-                    </div>
-                    <div className='input_box'>
-                        <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-
-                    </div>
-                    <div className='input_box'>
-                        <input type='email' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    </div>
-                </div>
-                <div className='formSwitch'>
-                <>
-                    Already a member? <span onClick={handleFormSwitch}>Login</span>
-                </>
-                </div>
-
-                <div className='submitContainer'>
-                    <div className='submit gray' onClick={handleFormSubmit}>
-                        Sign Up
-                    </div>
-                </div>
-
-            </div>            
-        )}
-
-
-        <Footer/>
+          <div className="submitContainer">
+            <div className="submit gray" onClick={handleFormSubmit}>
+              Login
+            </div>
+          </div>
         </div>
-        
-    );
+      ) : (
+        <div className="container">
+          <div className="signup_header">
+            <div className="signup_text">Sign up</div>
+          </div>
+
+          <div className="inputs_container">
+            <div className="input_box">
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="input_box">
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input_box">
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="input_box">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={confPassword}
+                onChange={(e) => setConfPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="formSwitch">
+            <>
+              Already a member? <span onClick={handleFormSwitch}>Login</span>
+            </>
+          </div>
+
+          <div className="submitContainer">
+            <div className="submit gray" onClick={handleFormSubmit}>
+              Sign Up
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Footer />
+    </div>
+  );
 };
 
 export default LoginSignup;
