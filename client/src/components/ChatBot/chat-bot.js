2e4e45f1d32bc6f285937053977129510fcc403e
Sragvi Vadali <reachsragvi@gmail.com>
2023-12-07 09:53:19 -0800

    Got getting journal entires working

--- client/src/components/ChatBot/chat-bot.js
+++ client/src/components/ChatBot/chat-bot.js
@@ -1,61 +1,76 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 
+import Header from "../Header/header";
+import Footer from "../Footer/footer";
+import "./chat-bot.css";
 
-import Header from '../Header/header';
-import Footer from '../Footer/footer';
-import './chat-bot.css';
-
-const ChatBot = ({ isSignedIn, setIsSignedIn}) => {
+const ChatBot = ({ isSignedIn, setIsSignedIn }) => {
   const [responses, setResponses] = useState([]);
 
-
   const handleContentChange = () => {
-    const currentContent = document.querySelector('.ask-box').innerText;
+    const currentContent = document.querySelector(".ask-box").innerText;
   };
 
-  const handleClick = async() => {
-    const currentContent = document.querySelector('.ask-box').innerText.trim();
-    console.log('Sending:', currentContent);
-
+  const handleClick = async () => {
+    const currentContent = document.querySelector(".ask-box").innerText.trim();
+    console.log("Sending:", currentContent);
 
     let answer = "";
-    try{
+    try {
       const config = {
         withCredentials: true,
         headers: {
           "Content-Type": "application/json",
         },
       };
-    const response = await axios.post(
-      "http://localhost:3001/chat/api/message",
-      {
-        input: currentContent
-      }, config);
+      const response = await axios.post(
+        "http://localhost:3001/chat/api/message",
+        {
+          input: currentContent,
+        },
+        config
+      );
       answer = response.data.answer;
+    } catch (error) {
+      const rephraseRequests = [
+        "Could you rephrase that, please?",
+        "If possible, rephrase the sentence.",
+        "I didn't quite catch that, can you rephrase it?",
+        "Can you express that differently?",
+        "Please provide an alternative wording.",
+        "Could you say that in a different way?",
+        "If it's convenient, could you rephrase?",
+        "I'm having trouble understanding, could you rephrase?",
+        "If you don't mind, rephrase the statement.",
+        "Could you give me another version of that?",
+        "Please elaborate or rephrase, if possible.",
+        "In other words, how would you say that?",
+        "Can you clarify by rephrasing?",
+        "I'm not sure I follow, can you reword that?",
+        "Can you express the idea in a different way?",
+        "If it's not too much trouble, rephrase your statement.",
+      ];
+      const randomRephraseRequest =
+        rephraseRequests[Math.floor(Math.random() * rephraseRequests.length)];
+      answer = rephraseRequests;
     }
-    catch(error){
-      answer = "Please rephrase that if possible."
-    }
-  
 
     //const formattedContent = currentContent.replace(/\n/g, "<br>");
 
     setResponses((prevResponses) => [
       ...prevResponses,
-      { id: 'User', text: currentContent },
-      {id: 'ChatBot', text: answer}
+      { id: "User", text: currentContent },
+      { id: "ChatBot", text: answer },
     ]);
 
-
-
-    //Get a response to the most recent message. 
+    //Get a response to the most recent message.
 
     // Add logic to send the message to the server and update the messages state
     // setMessages((prevMessages) => [...prevMessages, { id: generateId(), text: currentContent }]);
 
-    document.querySelector('.ask-box').innerText = '';
+    document.querySelector(".ask-box").innerText = "";
 
     // const messagesContainer = document.querySelector(".messages");
     // messagesContainer.scrollTop = messagesContainer.scrollHeight;
@@ -63,21 +78,21 @@ const ChatBot = ({ isSignedIn, setIsSignedIn}) => {
 
   return (
     <div className="page">
-      <Header isSignedIn={isSignedIn}/>
+      <Header isSignedIn={isSignedIn} />
       <div className="response-box">
         <div className="responses">
           {/* responses go here */}
           {responses.map((response) => (
             <div
               key={response.id}
-              className={
-                `response ${response.id === 'User' ? 'self' : 'other'} `
-              }
+              className={`response ${
+                response.id === "User" ? "self" : "other"
+              } `}
             >
               <div
-                className={
-                  `response-id ${response.id === 'User' ? 'self' : 'other'} `
-                }
+                className={`response-id ${
+                  response.id === "User" ? "self" : "other"
+                } `}
               >
                 {response.id}
               </div>
@@ -88,7 +103,11 @@ const ChatBot = ({ isSignedIn, setIsSignedIn}) => {
             </div>
           ))}
         </div>
-        <div className="ask-box" contentEditable="true" onInput={handleContentChange}></div>
+        <div
+          className="ask-box"
+          contentEditable="true"
+          onInput={handleContentChange}
+        ></div>
         <button className="submit-button" onClick={handleClick}>
           Send
         </button>
@@ -98,6 +117,6 @@ const ChatBot = ({ isSignedIn, setIsSignedIn}) => {
   );
 };
 
-ReactDOM.render(<ChatBot />, document.getElementById('root'));
+ReactDOM.render(<ChatBot />, document.getElementById("root"));
 
 export default ChatBot;
--- client/src/components/Journal/journal.js
+++ client/src/components/Journal/journal.js
@@ -1,14 +1,64 @@
-import { React, useState,useEffect } from "react";
-import { useNavigate, Route, Routes, Link,useParams} from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate, Route, Routes, Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
 import "./journal.css";
 
-const JournalEntry = ({title, timestamp, id }) => {
+// Create a new context
+const JournalContext = React.createContext();
 
+// Create a context provider component
+const JournalProvider = ({ children }) => {
+  const [journalEntries, setJournalEntries] = useState([]);
+
+  const getJournalEntries = async () => {
+    try {
+      const user = localStorage.getItem("userID");
+      const config = {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await axios.post(
+        "http://localhost:3001/journal/api/get-entries",
+        { userID: user },
+        config
+      );
+
+      const journalEntryMap = {};
+      response.data.forEach((entry) => {
+        const timeStampStr = String(entry.timeStamp);
+        journalEntryMap[timeStampStr] = entry;
+      });
+
+      setJournalEntries(journalEntryMap);
+    } catch (error) {
+      console.log("Error getting journal entries:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getJournalEntries();
+  }, []);
+
+  return (
+    <JournalContext.Provider value={{ journalEntries }}>
+      {children}
+    </JournalContext.Provider>
+  );
+};
+
+// Create a custom hook to use the context
+const useJournalContext = () => {
+  return useContext(JournalContext);
+};
+
+const JournalEntry = ({ title, timestamp, id }) => {
   const navigate = useNavigate();
+  const { journalEntries } = useJournalContext();
 
   const handleEntryClick = () => {
     navigate(`/journal/entry/${id}`);
@@ -22,182 +72,148 @@ const JournalEntry = ({title, timestamp, id }) => {
   );
 };
 
-
-const NewEntry= () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState(''); 
+const NewEntry = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const navigate = useNavigate();
 
   const config = {
     withCredentials: true,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   };
 
-  const handleSave = async() =>{
-    try{
-      const user = localStorage.getItem('userID');
+  const handleSave = async () => {
+    try {
+      const user = localStorage.getItem("userID");
 
-      await axios.post('http://localhost:3001/journal/api/add-entries',
-      {userID: user, time: Date.now(),entry:content, title:title},config);
-      navigate('/');
-    }catch(error){
-      console.error('error saving',error.message);
+      await axios.post(
+        "http://localhost:3001/journal/api/add-entries",
+        { userID: user, time: Date.now(), entry: content, title: title },
+        config
+      );
+      navigate("/");
+    } catch (error) {
+      console.error("error saving", error.message);
     }
-    window.location.href = '/journal';
+    window.location.href = "/journal";
   };
 
   return (
-    <div className = 'editor-page'>
-        <h2>New Journal Entry</h2>
+    <div className="editor-page">
+      <h2>New Journal Entry</h2>
 
-        <label className='title-editor'>Title:</label>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} className='title-input'/>
+      <label className="title-editor">Title:</label>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="title-input"
+      />
 
-        <div className="editor-buttons">
-          <button onClick={handleSave} >Save</button>
-         <Link to="/journal">
-           <button>Cancel</button>
-         </Link>
-        </div>
+      <div className="editor-buttons">
+        <button onClick={handleSave}>Save</button>
+        <Link to="/journal">
+          <button>Cancel</button>
+        </Link>
+      </div>
 
-        <label className='content-editor'>Content:</label>
-        <textarea value={content} onChange={(e) => setContent(e.target.value)} className='content-input'/>
+      <label className="content-editor">Content:</label>
+      <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="content-input"
+      />
 
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 };
 
-
-const JournalPage= ({isSignedIn}) => {
+const JournalPage = ({ isSignedIn }) => {
   const navigate = useNavigate();
-  const [journalEntries,setJournalEntries] = useState([]);
+  const { journalEntries } = useJournalContext();
 
-  const handleCreatenew = () => {
-    navigate('edit/new');
+  const handleCreateNew = () => {
+    navigate("edit/new");
   };
 
-  const config = {
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  useEffect(() => {
-    const getJournalEntries = async () => {
-      try {
-        const user = localStorage.getItem('userID');
-        const response = await axios.post(
-          'http://localhost:3001/journal/api/get-entries',
-          { userID: user },
-          config
-        );
-        setJournalEntries(response.data);  // Assuming the response contains an array of entries
-      } catch (error) {
-        console.log('Error getting journal entries:', error.message);
-      }
-    };
-
-    getJournalEntries();
-  },[]);  
-  
-  return(
+  return (
     <div className="journal-page">
-      <Header isSignedIn={isSignedIn}/>
+      <Header isSignedIn={isSignedIn} />
 
-      <div className='box-container'>
-        <div className="new-journal-entry" onClick={handleCreatenew}>
+      <div className="box-container">
+        <div className="new-journal-entry" onClick={handleCreateNew}>
           <h3>New Entry</h3>
           <div>+</div>
         </div>
 
         <div className="journal-entries">
-          {journalEntries.map((entry) => (
-            <JournalEntry key={entry.id} 
-            timestamp ={entry.timeStamp} 
-            id={entry.timeStamp}    
-            title={entry.title} />
+          {Object.entries(journalEntries).map(([id, entry]) => (
+            <JournalEntry
+              key={id}
+              timestamp={entry.timeStamp}
+              id={id}
+              title={entry.title}
+            />
           ))}
         </div>
-
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
-
-const ViewJournal = () => {
-  const id  = useParams();
-  const [content, setContent] = useState('');
-  const [title,setTitle] = useState('');
-
-  const config = {
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  useEffect(() =>{
-    const viewJournalContent = async() =>{
-      try{
-        const user = localStorage.getItem('userID');
-        const response = await axios.post(
-          'http://localhost:3001/journal/api/get-entries',
-          { userID: user },
-          config
-        );
-
-        for(let i = 0; i < response.data.length;i++){
-          const hash = response.data[i].timeStamp ;
-          if(toString(hash) === toString(id.id)){
-            setContent(response.data[i].entry);
-            setTitle(response.data[i].title);
-            break;
-          }
-        }
-
-      }catch(error){
-        console.log('Error getting journal entries:', error.message);
-      }
-    }
-
-    viewJournalContent();
-  },[]);
-
-  console.log(content);
-  console.log(title);
-  return(
-
-<div className='view-page'>
-  <div className='title-view-container'>
-    <div className="title-view">
-      {title}
-    </div>
-  </div>
-
-  <div className='content-view-container'>
-    <div className='content-view'>
-      {content}
-    </div>
-  </div>
-
-  <Footer />
-</div>
-  )
+  );
 };
 
-const Journal = ({isSignedIn}) => {  
-  return(
-    <Routes>
-      <Route path='/' element ={<JournalPage isSignedIn={isSignedIn} />}></Route>
-      <Route path='edit/new' element ={<NewEntry/>}/>
-      <Route path='entry/:id' element={<ViewJournal/>}/>
-    </Routes>
-  )
+const ViewJournal = () => {
+  const { id } = useParams();
+  const { journalEntries } = useJournalContext();
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (journalEntries[id]) {
+      setContent(journalEntries[id].entry);
+      setTitle(journalEntries[id].title);
+    }
+  }, [id, journalEntries]);
+
+  const handleBackClick = () => {
+    navigate("/journal");
+  };
+
+  return (
+    <div className="view-page">
+      <div className="title-view-container">
+        <div className="title-view">{title}</div>
+      </div>
+
+      <div className="content-view-container">
+        <div className="content-view">{content}</div>
+      </div>
+
+      <div className="back-button-container">
+        <button className="back-button" onClick={handleBackClick}>
+          &lt;
+        </button>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+const Journal = ({ isSignedIn }) => {
+  return (
+    <JournalProvider>
+      <Routes>
+        <Route path="/" element={<JournalPage isSignedIn={isSignedIn} />} />
+        <Route path="edit/new" element={<NewEntry />} />
+        <Route path="entry/:id" element={<ViewJournal />} />
+      </Routes>
+    </JournalProvider>
+  );
 };
 
 export default Journal;
